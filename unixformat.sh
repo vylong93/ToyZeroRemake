30dbfb9447482ebaa810c2c4b1584934db3e3fc2
Long Vy. Dang <vylong93khtn@gmail.com>
2020-12-02 23:35:23 +0700

    Add script unixformat to convert all headers and sources to UNIX standard
    Signed-off-by: Long Vy. Dang <vylong93khtn@gmail.com>

---
+++ unixformat.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dos2unix Makefile
+dos2unix STM32F103C8Tx_FLASH.ld
+cd Inc/
+for f in $(find . -name "*.h"); do dos2unix $f; done
+cd ..
+cd Src/
+for f in $(find . -name "*.c"); do dos2unix $f; done
+cd ..
