f2a5d75bb160abf2e2011ceae2e3799a6df116f3
Long Vy. Dang <vylong93khtn@gmail.com>
2021-02-15 22:42:10 +0700

    Generate:enable SPI2 and increase clock speed
    Signed-off-by: Long Vy. Dang <vylong93khtn@gmail.com>

--- Inc/main.h
+++ Inc/main.h
@@ -60,6 +60,8 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define LED_Pin GPIO_PIN_13
 #define LED_GPIO_Port GPIOC
+#define WS2812B_Pin GPIO_PIN_15
+#define WS2812B_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
